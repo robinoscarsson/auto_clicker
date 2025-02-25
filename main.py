493b6677e201caeb5b09b963eb59fc95a9954e43
Robin Oscarsson <robin.oscarsson@gmail.com>
2025-02-25 23:02:51 +0100

    Replacing logging with print to remove ugly bloaty boilerplate code

--- main.py
+++ main.py
@@ -1,24 +1,6 @@
 from pynput import keyboard, mouse
-import logging
 import threading
 import time
-
-# create logger
-logger = logging.getLogger('simple_example')
-logger.setLevel(logging.DEBUG)
-
-# create console handler and set level to debug
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-
-# create formatter
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-
-# add formatter to ch
-ch.setFormatter(formatter)
-
-# add ch to logger
-logger.addHandler(ch)
 
 mouse_controller = mouse.Controller()
 
@@ -37,18 +19,18 @@ def on_press(key):
             is_mouse_clicking = not is_mouse_clicking
             if is_mouse_clicking:
                 threading.Thread(target=simulate_mouse_click).start()
-            logger.info(f"Mouse clicking {'started' if is_mouse_clicking else 'stopped'}")
+            print(f"Mouse clicking {'started' if is_mouse_clicking else 'stopped'}")
     except AttributeError:
-        logger.info(f"Special key pressed: {key}")
+        print(f"Special key pressed: {key}")
 
 def on_release(key):
     if key == keyboard.Key.esc:
         # Stop listener
-        logger.info("Stopping listener")
+        print("Stopping listener")
         return False
 
 def main(*args, **kwargs):
-    logger.info("Starting autoclicker. Press F1 to start/stop clicking. Press ESC to exit.")
+    print("Starting autoclicker. Press F1 to start/stop clicking. Press ESC to exit.")
     with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
         listener.join()
 
